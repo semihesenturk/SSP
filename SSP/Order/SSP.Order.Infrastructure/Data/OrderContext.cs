99758bf904af38dfa02082ad1340febc7aa143f3
semihesenturkdev <semih.esenturk@dygdigital.com>
2022-02-14 22:55:22 +0300

    Order Context has added

---
+++ SSP/Order/SSP.Order.Infrastructure/Data/OrderContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SSP.Order.Infrastructure.Data
+{
+    public class OrderContext : DbContext
+    {
+        #region Constructor
+        public OrderContext(DbContextOptions options)
+           : base(options)
+        {
+
+        }
+        #endregion
+
+        #region Db Sets
+        public DbSet<Domain.Entities.Order> Orders { get; set; } 
+        #endregion
+    }
+}
