be909c1f3ea1345cc30a5bb4371d3615ae07549c
semihesenturkdev <semih.esenturk@dygdigital.com>
2022-02-14 08:35:00 +0300

    Order project arch has created

---
+++ SSP/Order/SSP.Order.API/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SSP.Order.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+    }
+}
