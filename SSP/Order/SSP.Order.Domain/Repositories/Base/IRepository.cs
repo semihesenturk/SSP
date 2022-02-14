33cc984551446aa896a6479df15147ac9271b073
semihesenturkdev <semih.esenturk@dygdigital.com>
2022-02-14 09:19:05 +0300

    generic repository implementation for domain layer

---
+++ SSP/Order/SSP.Order.Domain/Repositories/Base/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using SSP.Order.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace SSP.Order.Domain.Repositories.Base
+{
+    public interface IRepository<T> where T : Entity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate);
+
+        Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeString = null,
+            bool disableTracking = true);
+
+        Task<T> GetByIdAsync(int id);
+
+        Task<T> AddASync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ SSP/Order/SSP.Order.Domain/Repositories/IOrderRepository.cs
@@ -0,0 +1,8 @@
+﻿using SSP.Order.Domain.Repositories.Base;
+
+namespace SSP.Order.Domain.Repositories
+{
+    public interface IOrderRepository : IRepository<Entities.Order>
+    {
+    }
+}
