a0b81cf09e620cfb028db547ec93e9d94b43063c
semihesenturkdev <semih.esenturk@dygdigital.com>
2022-03-06 19:57:32 +0300

    Order API environment based settings files has added.

--- SSP/Order/SSP.Order.API/Program.cs
+++ SSP/Order/SSP.Order.API/Program.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using SSP.Order.API.Extensions;
+using System;
+using System.IO;
 
 namespace SSP.Order.API
 {
     public class Program
     {
+        private static IConfiguration Configuration
+        {
+            get
+            {
+                string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+                return new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false)
+                    .AddJsonFile($"appsettings.{env}.json", optional: true)
+                    .AddEnvironmentVariables()
+                    .Build();
+            }
+        }
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build()
@@ -25,6 +42,7 @@ namespace SSP.Order.API
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseConfiguration(Configuration);
                     webBuilder.UseStartup<Startup>();
                 });
     }
